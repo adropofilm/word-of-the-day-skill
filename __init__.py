1ac3e0721973b2ef45d916e463abf98949a0f090
Fatima Mohamed <adropofilm@gmail.com>
2018-04-26 10:52:48 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -31,8 +31,10 @@ __author__ = 'fmohamed'
 
 LOGGER = getLogger(__name__)
 
-# "replaces" values in given string using dictionary key/value pairs.
 def replace_all(text, dictionary):
+    """
+    "replaces" values in given string using dictionary key/value pairs.
+    """
     for i, j in dictionary.iteritems():
         text = text.replace(i,j)
     return text
